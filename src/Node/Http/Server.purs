f835bab3218a27bbf07fbf3947e97790a7bcc04b
Branimir Klarić <branimir.klaric.bk@gmail.com>
2018-04-07 18:05:10 +0200

    Replace Network dependency with Server

--- src/Node/Http/Server.purs
+++ src/Node/Http/Server.purs
@@ -1,5 +1,5 @@
 module Node.Http.Server
-    ( HttpServerImpl
+    ( HttpServer
     , Request
     , Response
     , create
@@ -8,18 +8,34 @@ module Node.Http.Server
 import Prelude
 
 import Control.Monad.Effect (Effect)
+import Node.Events.EventEmitter as EventEmitter
+import Node.Events.EventEmitter (class EventEmitter)
 import Node.Http.ServerRequest (ServerRequest)
 import Node.Http.ServerResponse (ServerResponse)
-import Node.Net.Server (class NetServer, defaultListen)
+import Node.Server (class Server, defaultListen)
 
-foreign import data HttpServerImpl :: Type
+foreign import data HttpServer :: Type
 
 type Request = ServerRequest
 
 type Response = ServerResponse
 
 foreign import create ::
-    (Request -> Response -> Effect Unit) -> Effect HttpServerImpl
+    (Request -> Response -> Effect Unit) -> Effect HttpServer
 
-instance netServerHttpServerImpl :: NetServer HttpServerImpl where
+instance eventEmitterHttpServer :: EventEmitter HttpServer where
+    on                  = EventEmitter.defaultOn
+    once                = EventEmitter.defaultOnce
+    prependListener     = EventEmitter.defaultPrependListener
+    prependOnceListener = EventEmitter.defaultPrependOnceListener
+    removeListener      = EventEmitter.defaultRemoveListener
+    removeAllListeners  = EventEmitter.defaultRemoveAllListeners
+    emit                = EventEmitter.defaultEmit
+    listeners           = EventEmitter.defaultListeners
+    listenerCount       = EventEmitter.defaultListenerCount
+    getMaxListeners     = EventEmitter.defaultGetMaxListeners
+    setMaxListeners     = EventEmitter.defaultSetMaxListeners
+    eventNames          = EventEmitter.defaultEventNames
+
+instance serverHttpServer :: Server HttpServer where
     listen = defaultListen
--- test/Main.purs
+++ test/Main.purs
@@ -3,11 +3,21 @@ module Test.Main where
 import Prelude
 
 import Control.Monad.Effect (Effect)
+import Control.Monad.Effect.Ref (Ref, modifyRef, newRef, readRef)
+import Control.Monad.Except (runExcept)
+import Data.Array (cons)
+import Data.Either (Either, either)
+import Data.Foreign (Foreign, ForeignError, unsafeFromForeign, unsafeReadTagged)
+import Data.List.Types (NonEmptyList)
 import Data.Maybe (Maybe(..))
+import Node.Buffer (Buffer, concat_)
+import Node.Events.EventEmitter (on')
 import Node.Http.Server (Request, Response, create)
 import Node.Http.ServerRequest (method, url)
-import Node.Net.Server (ListenOptions(..), listen_)
-import Node.Stream.Readable (pipe_)
+import Node.Http.ServerResponse (setHeader, setStatusCode)
+import Node.Server (ListenOptions(..), listen_)
+import Node.Stream.Readable.Events (data', end)
+import Node.Stream.Writable (endBuffer_, writeString__)
 
 listenOptions :: ListenOptions
 listenOptions = TcpListenOptions
@@ -17,12 +27,25 @@ listenOptions = TcpListenOptions
     , exclusive: Nothing
     }
 
+foreignToBuffer :: Foreign -> Either (NonEmptyList ForeignError) Buffer
+foreignToBuffer foreignBuffer =
+    unsafeReadTagged "Uint8Array" foreignBuffer
+    # runExcept
+    <#> unsafeFromForeign
+
 requestHandler :: Request -> Response -> Effect Unit
 requestHandler request response = do
     let method' = method request
     let url' = url request
-    method
+    setStatusCode 500 response
+    setHeader "nigga" "higga" response
+    response # writeString__ ("<p>Method is " <> method' <> " and url is " <> url' <> "</p>") # void
+    (body :: Ref (Array Buffer)) <- newRef []
+    _ <- request # on' data' \foreignData ->
+        foreignToBuffer foreignData # either (const $ pure unit) \buffer ->
+            modifyRef body (cons buffer)
+    _ <- request # on' end (readRef body >>= concat_ >>= (flip endBuffer_ response) # void)
+    pure unit
 
 main :: Effect Unit
-main = create (\request response -> request # pipe_ response # void)
-    >>= listen_ listenOptions
+main = create requestHandler >>= listen_ listenOptions
