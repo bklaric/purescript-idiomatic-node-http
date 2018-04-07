e8774a1e32c8e44bdd39f0c9c420d95c18196db1
Branimir Klarić <branimir.klaric.bk@gmail.com>
2018-04-07 16:45:27 +0200

    Add purescript-idiomatic-node-server

--- test/Main.purs
+++ test/Main.purs
@@ -4,7 +4,8 @@ import Prelude
 
 import Control.Monad.Effect (Effect)
 import Data.Maybe (Maybe(..))
-import Node.Http.Server (create)
+import Node.Http.Server (Request, Response, create)
+import Node.Http.ServerRequest (method, url)
 import Node.Net.Server (ListenOptions(..), listen_)
 import Node.Stream.Readable (pipe_)
 
@@ -15,6 +16,12 @@ listenOptions = TcpListenOptions
     , backlog: Nothing
     , exclusive: Nothing
     }
+
+requestHandler :: Request -> Response -> Effect Unit
+requestHandler request response = do
+    let method' = method request
+    let url' = url request
+    method
 
 main :: Effect Unit
 main = create (\request response -> request # pipe_ response # void)
