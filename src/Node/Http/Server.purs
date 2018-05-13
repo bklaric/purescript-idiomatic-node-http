bd202709f08b0791bd72dffbebfbda5d1b5bc23f
Branimir Klarić <branimir.klaric.bk@gmail.com>
2018-05-13 18:00:54 +0200

    Require request and response error handlers when creating server

--- src/Node/Http/Server.purs
+++ src/Node/Http/Server.purs
@@ -6,16 +6,34 @@ module Node.Http.Server
 import Prelude
 
 import Effect (Effect)
+import Node.Errors (Error)
+import Node.Events.EventEmitter (class EventEmitter, on)
 import Node.Events.EventEmitter as EventEmitter
-import Node.Events.EventEmitter (class EventEmitter)
+import Node.Events.EventListener (toEventListener)
+import Node.Http.IncomingMessage as IncomingMessage
 import Node.Http.Server.Request (Request)
 import Node.Http.Server.Response (Response)
+import Node.Http.Server.Response as Response
 import Node.Server (class Server, defaultListen)
 
 foreign import data HttpServer :: Type
 
-foreign import create ::
+foreign import createImpl ::
     (Request -> Response -> Effect Unit) -> Effect HttpServer
+
+create
+    :: (Error -> Effect Unit)
+    -> (Error -> Effect Unit)
+    -> (Request -> Response -> Effect Unit)
+    -> Effect HttpServer
+create requestErrorListener responseErrorListener callback =
+    createImpl \request response -> let
+        requestErrorListener' = toEventListener requestErrorListener
+        responseErrorListener' = toEventListener responseErrorListener
+        in do
+        request # on IncomingMessage.error requestErrorListener' # void
+        response # on Response.error responseErrorListener' # void
+        callback request response
 
 instance eventEmitterHttpServer :: EventEmitter HttpServer where
     on                  = EventEmitter.defaultOn
